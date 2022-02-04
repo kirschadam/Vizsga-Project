7724c04d7734704ff95225ebcc06a184f0de00ce
Kecskeprojects <kirsch.a.p@gmail.com>
2022-02-04 20:20:51 +0100

    Storage Classes

--- Backend/Base service/Interfaces/ILocationService.cs
+++ Backend/Base service/Interfaces/ILocationService.cs
@@ -1,10 +1,9 @@
-﻿using System.Collections.Generic;
+﻿using Base_service.JsonClasses;
 using System.Runtime.InteropServices;
-using System.Runtime.Serialization;
 using System.ServiceModel;
 using System.ServiceModel.Web;
 
-namespace Base_service
+namespace Base_service.Interfaces
 {
     [ServiceContract]
     public interface ILocationService
@@ -80,143 +79,5 @@ namespace Base_service
             UriTemplate = "/deleteregion?uid={uid}&id={id}&region={region}"
             )]
         string RemoveRegion(string uid, [Optional] string id, [Optional] string region);
-    }
-
-    [DataContract]
-    public class Response_Region
-    {
-        private string message = null;
-        private List<Region> regions = new List<Region>();
-
-        [DataMember]
-        public string Message
-        {
-            get { return message; }
-            set { message = value; }
-        }
-
-        [DataMember]
-        public List<Region> Regions
-        {
-            get { return regions; }
-            set { regions = value; }
-        }
-
-        public Response_Region() { }
-
-        public Response_Region(string message, List<Region> regions)
-        {
-            Message = message;
-            Regions = regions;
-        }
-    }
-
-
-
-    [DataContract]
-    public class Region
-    {
-        private int? id = null;
-        private string name = null;
-        private List<Store> locations = new List<Store>();
-
-        [DataMember]
-        public int? Id
-        {
-            get { return id; }
-            set { id = value; }
-        }
-
-        [DataMember]
-        public string Name
-        {
-            get { return name; }
-            set { name = value; }
-        }
-
-        [DataMember]
-        public List<Store> Locations
-        {
-            get { return locations; }
-            set { locations = value; }
-        }
-
-        public Region() { }
-
-        public Region(int? id, string name)
-        {
-            Id = id;
-            Name = name;
-        }
-    }
-
-
-
-    [DataContract]
-    public class Response_Location
-    {
-        private string message = null;
-        private List<Store> locations = new List<Store>();
-
-        [DataMember]
-        public string Message 
-        { 
-            get { return message; } 
-            set { message = value; } 
-        }
-
-        [DataMember]
-        public List<Store> Locations
-        {
-            get { return locations; }
-            set { locations = value; }
-        }
-
-        public Response_Location() { }
-
-        public Response_Location(string message, List<Store> locations)
-        {
-            Message = message;
-            Locations = locations;
-        }
-    }
-
-
-
-    [DataContract]
-    public class Store
-    {
-        private int? id = null;
-        private string name = null, region = null;
-
-        [DataMember]
-        public int? Id
-        {
-            get { return id; }
-            set { id = value; }
-        }
-
-        [DataMember]
-        public string Name
-        {
-            get { return name; }
-            set { name = value; }
-        }
-
-        [DataMember]
-        public string Region
-        {
-            get { return region; } 
-            set { region = value; }
-        }
-
-        public Store() { }
-
-        public Store(int? id, string name, string region)
-        {
-            Id = id;
-            Name = name;
-            Region = region;
-        }
     }
 }
